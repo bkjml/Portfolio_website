916354f75263b086873142c30d1441c9509818a3
Matiyas Birhane <mbirhane@kifiya.et>
2024-08-10 12:34:30 +0300

    hero section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,10 +4,23 @@ import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponent
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
+const handleEmailClick = () => {
+  window.location.href="mailto:mattbirhane@gmail.com?subject=Get to Know You More&body=Hi Mathias,";
+}
+
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome To <br />
+        My Personal Portfolio
+      </SectionTitle>
+      <SectionText>
+        Web3 and Blockchain Enthusiast | Proficient in React & Scalable Solutions Design | Java & Python Backend Developer
+      </SectionText>
+      <Button onClick={handleEmailClick}>Learn More</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
