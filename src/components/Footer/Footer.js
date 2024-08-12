4329c9ebfcebdee237d5438d523aabb40db48ad9
Matiyas Birhane <mbirhane@kifiya.et>
2024-08-12 23:12:22 +0300

    footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,9 +6,39 @@ import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTi
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <FooterWrapper>
+      <LinkList>
+        <LinkColumn>
+          <LinkTitle>
+          Call
+          </LinkTitle>
+          <LinkItem href='tel:111-111-1111'>111-111-1111</LinkItem>
+        </LinkColumn>
+        <LinkColumn>
+          <LinkTitle>
+          Email
+          </LinkTitle>
+          <LinkItem href='mailto:mattbirhane@gmail.com'>mattbirhane@gmail.com</LinkItem>
+        </LinkColumn>
+
+      </LinkList>
+      <SocialIconsContainer>
+        <CompanyContainer>
+          <Slogan>Innovating one project at a time</Slogan>
+        </CompanyContainer>
+        <SocialContainer>
+          <SocialIcons href='https://github.com/bkjml'>
+            <AiFillGithub size="3rem"/>
+          </SocialIcons>
+          <SocialIcons href='https://linkedin.com/in/mathias-birhane'>
+            <AiFillLinkedin size="3rem"/>
+          </SocialIcons>
+          <SocialIcons href='https://x.com/Matt_BPan'>
+            <AiFillTwitterCircle size="3rem"/>
+          </SocialIcons>
+        </SocialContainer>
+      </SocialIconsContainer>
+    </FooterWrapper>
   );
 };
 
